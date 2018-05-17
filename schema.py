a55e2fd6ddd05905e7d564a3eccad278d1fc7567
Jake <jakemacnaughton@gmail.com>
2018-05-17 15:13:42 -0400

    took out solutions

--- schema.py
+++ schema.py
@@ -5,14 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    capital_city = Column(String)
-    population = Column(Integer)
-    landlocked = Column(Boolean)
-
+    # CREATE THE SCHEMA HERE
 
 engine = create_engine('sqlite:///states.db', echo=True)
 Base.metadata.create_all(engine)
